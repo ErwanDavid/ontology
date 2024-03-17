fd7c13310713020521f57b46314ee9f42f3f62b3
ErwanDavid <erwan.dd@gmail.com>
2024-03-17 11:44:45 +0100

    staring on fastapi

--- src/getOBO.py
+++ src/getOBO.py
@@ -15,7 +15,7 @@ for onto in ontologyList:
     if onto['id'] in importantOnto :
         print("Find ",onto['id'], "title",onto['title'])
         for product in onto['products']:
-            idStr = './owl_file/' + onto['id'] + ".owl"
+            idStr = '../owl_file/' + onto['id'] + ".owl"
             if product['id'] == onto['id'] + ".owl" :
                 print("DL OWL URL", product['ontology_purl'])
                 urllib.request.urlretrieve(product['ontology_purl'], idStr)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from urllib.request import urlopen
+import urllib.request
+import json
+import os
 
 app = FastAPI()
+
+
+response = urlopen("https://obofoundry.org/registry/ontologies.jsonld")
+ontologyListJson = response.read()
+ontologyList = json.loads(ontologyListJson)['ontologies']
 
 @app.get('/')
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get('/list_owl')
+def list_owl():
+    return os.listdir("/code/owl_file/")
+
+@app.get('/add_owl/{onto_code}')
+def add_owl(onto_code):
+    return {"add_owl": onto_code}
